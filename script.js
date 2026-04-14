113bc664ca7331911978eb5f9d29861f7e96a3e6
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-04-15 04:03:54 +0500

    v0.0.0.0.3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const playerTank = document.getElementById("playerTank")
 const playerTankTurret = document.getElementById("playerTankTurret")
-const speed = 4;
+const speed = 6;
 const rotateTank = 6;
 const rotateTurret = 8;
 let speedX = 0;
@@ -41,13 +41,59 @@ function tick(time){
     tick.count = (tick.count || 0)
     if(time - (tick.lastTime || 0) >= 100){
         for (const key in keysDown){
-            if(key == "W" && keysDown[key]){curPositionY -= speed} 
-            if(key == "S" && keysDown[key]){curPositionY += speed} 
-            if(key == "A" && keysDown[key]){curPositionX -= speed} 
-            if(key == "D" && keysDown[key]){curPositionX += speed} 
+            if(key == "W" && keysDown[key]){
+                let degrees = (360 + curTankRotate) % 360;
+                let X;
+                let Y;
+                console.log(degrees)
+                let fixYgol = curTankRotate % 90;
+                if(fixYgol === 0){fixYgol = 90};
+                if(degrees > 0 && degrees <= 90){X = 1; Y = -1;
+                    curPositionY += (1 / 90 * (90 - fixYgol) * speed) * Y; 
+                    curPositionX += (1 / 90 * fixYgol * speed) * X;
+                }
+                else if (degrees > 90 && degrees <= 180){X = 1; Y = 1;
+                    curPositionY += (1 / 90 * fixYgol * speed) * Y; 
+                    curPositionX += (1 / 90 * (90 - fixYgol) * speed) * X;
+                }
+                else if (degrees > 180 && degrees <= 270){X = -1; Y = 1;
+                    curPositionY += (1 / 90 * (90 - fixYgol) * speed) * Y; 
+                    curPositionX += (1 / 90 * fixYgol * speed) * X;
+                }
+                else if (degrees > 270 || degrees == 0){X = -1; Y = -1;
+                    curPositionY += (1 / 90 * fixYgol * speed) * Y; 
+                    curPositionX += (1 / 90 * (90 - fixYgol) * speed) * X;
+                }
+            } 
+            if(key == "S" && keysDown[key]){
+                let degrees = (360 + curTankRotate) % 360;
+                let X;
+                let Y;
+                console.log(degrees)
+                let fixYgol = curTankRotate % 90;
+                if(fixYgol === 0){fixYgol = 90};
+                if(degrees > 0 && degrees <= 90){X = -1; Y = 1;
+                    curPositionY += (1 / 90 * (90 - fixYgol) * speed) * Y; 
+                    curPositionX += (1 / 90 * fixYgol * speed) * X;
+                }
+                else if (degrees > 90 && degrees <= 180){X = -1; Y = -1;
+                    curPositionY += (1 / 90 * fixYgol * speed) * Y; 
+                    curPositionX += (1 / 90 * (90 - fixYgol) * speed) * X;
+                }
+                else if (degrees > 180 && degrees <= 270){X = 1; Y = -1;
+                    curPositionY += (1 / 90 * (90 - fixYgol) * speed) * Y; 
+                    curPositionX += (1 / 90 * fixYgol * speed) * X;
+                }
+                else if (degrees > 270 || degrees == 0){X = 1; Y = 1;
+                    curPositionY += (1 / 90 * fixYgol * speed) * Y; 
+                    curPositionX += (1 / 90 * (90 - fixYgol) * speed) * X;
+                }
+            } 
+            if(key == "A" && keysDown[key]){curTankRotate -= rotateTank} 
+            if(key == "D" && keysDown[key]){curTankRotate += rotateTank} 
             if(key == "Z" && keysDown[key]){curTurretRotate -= rotateTurret} 
             if(key == "X" && keysDown[key]){curTurretRotate += rotateTurret}
-            playerTank.style.transform = "translate("+curPositionX+"px, "+curPositionY+"px)";
+            playerTank.style.transform = "translate("+curPositionX +"px, "+curPositionY+"px) rotate("+curTankRotate+"deg)";
             playerTankTurret.style.transform = "rotate("+curTurretRotate+"deg)";
         }
 
