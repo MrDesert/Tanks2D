9a95d3019569f4cc3f0c22fe33def318801eff63
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-04-18 00:15:30 +0500

    v0.0.0.0.8.2

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const app = express();
+const app = express(); //само приложение
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Сервер работает');
-});
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Счётчик подключений</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1>Клиентская страница</h1>
+        <p>Здесь позже появится номер подключения</p>
+      </body>
+    </html>
+  `;
+  res.send(html);
+}); //Когда кто то заходить на сервер выводит
 
 app.listen(port, () => {
   console.log(`Сервер запущен на порту ${port}`);
-});
+}); // пишет в консоль сервере на каком порту он запущен
