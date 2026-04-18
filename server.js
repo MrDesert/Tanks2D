a4cec9df0b16711260df000ed9d1904b4d266fbd
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-04-18 14:38:33 +0500

    v0.0.0.0.9.4

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const http = require('http');
 const WebSocket = require('ws');
+const cookieParser = require('cookie-parser');
+const crypto = require('crypto');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cookieParser());
 
 let totalConnections = 0;
 let activeConnections = 0;
 
 app.get('/', (req, res) => {
+  let userId = req.cookies.userId;
+  if (!userId) {
+    userId = crypto.randomUUID();
+    res.cookie('userId', userId, { httpOnly: true, maxAge: 365 * 24 * 60 * 60 * 1000 });
+  }
+
   const html = `
     <!DOCTYPE html>
     <html>
