4528636fde67b17c59b125be2993ab4a4ebdad85
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-04-20 01:44:57 +0500

    v0.0.0.1.2.9

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ wss.on('connection', (ws, req) => {
 
         for(let key in mapObj.walls){
           if(data.positionY < 100){
-            console.log(key)
+            console.log(key.Top)
           }
           if(data.positionX+43 > (key.Left) && 
             data.positionX < (key.Left+key.Width) && 
