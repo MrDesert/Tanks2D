692a36cb6a4afe6a32788729b3e3df6120fc2b13
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-04-20 20:52:09 +0500

    v0.0.0.1.2.23

--- server.js
+++ server.js
@@ -96,23 +96,24 @@ wss.on('connection', (ws, req) => {
         });
       } else if(data.type === 'ping'){
          ws.send(JSON.stringify({ type: 'pong', clientTime: data.clientTime, serverTime: Date.now()}));
-      } else if (data.type === 'keysDown'){
-          if(data.W){
-            const radian = ws.tankRotate * Math.PI / 180;
-            ws.positionX += tankSpeed * Math.sin(radian);
-            ws.positionY -= tankSpeed * Math.cos(radian);
-          };
-          if(data.S){
-            const radian = ws.tankRotate * Math.PI / 180;
-            ws.positionX -= tankSpeed * Math.sin(radian);
-            ws.positionY += tankSpeed * Math.cos(radian);
-          };
-          if(data.A){ws.tankRotate -= rotateTank;};
-          if(data.D){ws.tankRotate += rotateTank;};
-          if(data.Z){ws.turretRotate -= rotateTurret;} 
-          if(data.X){ws.turretRotate += rotateTurret;}
-          ws.send(JSON.stringify({ type: 'movement', turretRotate: ws.turretRotate, tankRotate: ws.tankRotate, positionX: ws.positionX, positionY: ws.positionY}));
-      }
+      } 
+      // else if (data.type === 'keysDown'){
+      //     if(data.W){
+      //       const radian = ws.tankRotate * Math.PI / 180;
+      //       ws.positionX += tankSpeed * Math.sin(radian);
+      //       ws.positionY -= tankSpeed * Math.cos(radian);
+      //     };
+      //     if(data.S){
+      //       const radian = ws.tankRotate * Math.PI / 180;
+      //       ws.positionX -= tankSpeed * Math.sin(radian);
+      //       ws.positionY += tankSpeed * Math.cos(radian);
+      //     };
+      //     if(data.A){ws.tankRotate -= rotateTank;};
+      //     if(data.D){ws.tankRotate += rotateTank;};
+      //     if(data.Z){ws.turretRotate -= rotateTurret;} 
+      //     if(data.X){ws.turretRotate += rotateTurret;}
+      //     ws.send(JSON.stringify({ type: 'movement', turretRotate: ws.turretRotate, tankRotate: ws.tankRotate, positionX: ws.positionX, positionY: ws.positionY}));
+      // }
     } catch (err) {
       console.error('Ошибка парсинга сообщения:', err);
     }
