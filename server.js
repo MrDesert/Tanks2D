4118d1b2c79439db8f46953fa2ed276d05465bce
MrDesert <zzz-x.x.x-zzz@yandex.ru>
2026-04-24 23:18:54 +0500

    v0.0.0.1.5.19

--- server.js
+++ server.js
@@ -392,7 +392,7 @@ function broadcastBullets() {
 
 setInterval(moveBullet, 100)
 function moveBullet(){          
-const bulletSpeed = 4;
+const bulletSpeed = 5;
 const toDelete = [];
 
 for (let [id, bullet] of bullets) {
